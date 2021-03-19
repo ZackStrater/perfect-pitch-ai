4d6ed744d35b8270338dfd7217de66bd5a15e468
Zack Strater <zackstrater@Zack.Strater.Lenovo>
2021-03-18 21:11:58 -0700

    finalizing midi from numpy array

--- src/midi_to_csv.py
+++ src/midi_to_csv.py
@@ -5,9 +5,12 @@ import sys
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
+np.set_printoptions(suppress=True)
 #np.set_printoptions(threshold=np.inf)
 
 csv_string = pm.midi_to_csv('../data/test_midi_csv.midi')
+
+print(csv_string)
 
 meta_data = csv_string[0:7]
 track_end = csv_string[-2:]
@@ -38,11 +41,10 @@ midi_note_array = np.zeros((127, song_total_ticks))
 
 mask = df['control'] == ' Note_on_c'
 df_notes = df[mask]
-print(df_notes)
 df_pedals = df[~mask]
 df_sustain = df_pedals[df_pedals['control_num'] == 64]
 df_soft = df_pedals[df_pedals['control_num'] == 67]
-
+print(df_notes.head(50))
 
 df_sorted_notes = df_notes.sort_values(['control_num', 'tick']).reset_index(drop=True)
 df_key_lift = df_sorted_notes[df_sorted_notes['velocity'] == 0].reset_index()
@@ -51,45 +53,28 @@ df_key_press = df_sorted_notes[df_sorted_notes['velocity'] != 0].reset_index()
 assert df_key_lift.shape[0] == df_key_press.shape[0]
 assert df_key_press['control_num'].equals(df_key_lift['control_num'])
 
+
 df_note_durations = pd.DataFrame({'start_tick': df_key_press['tick'], 'end_tick': df_key_lift['tick'],
                                   'control_num': df_key_press['control_num'], 'velocity': df_key_press['velocity']})
 
-print(df_note_durations)
 
 def map_note(array, note_value, note_start, note_end, velocity):
-    array[note_value, note_start:note_end+1] = velocity
+    # numpy slice isn't inclusive of note_end,
+    # but it works b/c note end is the first moment the key is not longer pressed
+    array[note_value, note_start:note_end] = velocity
 
 
 for idx, row in df_note_durations.iterrows():
     map_note(midi_note_array, row['control_num'], row['start_tick'], row['end_tick'], row['velocity'])
 
-
-def write_midi_line(track, tick, control, channel, control_num, velocity):
-    midi_string = ', '.join([str(track), str(tick), str(control), str(channel), str(control_num), str(velocity)])
-    midi_string += '\n'
-    return midi_string
-
 # print(meta_data)
 # print(write_midi_line(2, 447165, 'Control_c', 0, 64, 0))
 # print(track_end)
 
-# x, y = np.nonzero(midi_note_array)
-# print(x)
-# print(y)
-# print(y.shape)
-
-
-def zero_runs(a):
-    # Create an array that is 1 where a is 0, and pad each end with an extra 0.
-    iszero = np.concatenate(([0], np.equal(a, 0).view(np.int8), [0]))
-    absdiff = np.abs(np.diff(iszero))
-    # Runs start and end where absdiff is 1.
-    ranges = np.where(absdiff == 1)[0].reshape(-1, 2)
-    return ranges
 
 print('\n\n\n\n\n\n')
 
-def find_runs(x):
+def find_runs(x, row_index):
     n = x.shape[0]
     loc_run_start = np.empty(n, dtype=bool)
     loc_run_start[0] = True
@@ -108,30 +93,50 @@ def find_runs(x):
     note_lengths = run_lengths[notes_mask]
     note_velocities = run_values[notes_mask]
 
-    return note_starts, note_lengths, note_velocities
+    # need to make a np array with note value (passed in as row_index) that is the same length as
+    # the number of notes found for that row
+    note_values = np.full(len(note_starts), row_index)
+    return np.dstack((note_values, note_starts, note_lengths, note_velocities))[0]
 
 print('\n\n\n\n\n\n')
 
-print(find_runs(midi_note_array[55]))
-print(midi_note_array[55, 73381:(73381+67)])
+# print(find_runs(midi_note_array[55], 55))
+# print(midi_note_array[55, 73381:(73381+66)])
+# print(midi_note_array[31, 292367:(292367+47)])
+# print(midi_note_array[31, (292367+4)])
+
+note_presses = np.vstack([find_runs(row, idx) for idx, row in enumerate(midi_note_array)])
+note_releases = note_presses.copy()
+
+# key lifts are calculated by taking note press tick and adding note duration
+# key lift occurs after note duration end (start + length + 1)
+note_releases[:, 1] += note_releases[:, 2]
+# note length and velocity is 0 for key lift
+note_releases[:, 2:4] = 0
+
+presses_and_releases = np.vstack([note_presses, note_releases])
+print(presses_and_releases)
+sorted_presses_and_releases = presses_and_releases[presses_and_releases[:, 1].argsort()]
+print(sorted_presses_and_releases)
+
+def write_midi_line(track, tick, control, channel, control_num, velocity):
+    midi_string = ', '.join([str(track), str(tick), str(control), str(channel), str(control_num), str(velocity)])
+    midi_string += '\n'
+    return midi_string
 
 
 
+midi_out = []
+for line in meta_data:
+    midi_out.append(line)
+for line in sorted_presses_and_releases:
+#   track    tick     control  channel  control_num  velocity
+    midi_out.append(write_midi_line(2, int(line[1]), 'Note_on_c', 0, int(line[0]), int(line[3])))
+for line in track_end:
+    midi_out.append(line)
+print(midi_out)
 
-# midi_out = []
-# for line in meta_data:
-#     midi_out.append(line)
-# midi_out.append(write_midi_line(2, 0, 'Note_on_c', 0, 50, 60))
-# midi_out.append(write_midi_line(2, 1000, 'Note_on_c', 0, 50, 0))
-# midi_out.append(write_midi_line(2, 1000, 'Note_on_c', 0, 52, 60))
-# midi_out.append(write_midi_line(2, 2000, 'Note_on_c', 0, 52, 0))
-# midi_out.append(write_midi_line(2, 2000, 'Note_on_c', 0, 53, 60))
-# midi_out.append(write_midi_line(2, 3000, 'Note_on_c', 0, 53, 0))
-# for line in track_end:
-#     midi_out.append(line)
-# print(midi_out)
-#
-# midi_object = pm.csv_to_midi(midi_out)
-# with open('../data/testing_midi_io.mid', 'wb') as output_file:
-#     midi_writer = pm.FileWriter(output_file)
-#     midi_writer.write(midi_object)
+midi_object = pm.csv_to_midi(midi_out)
+with open('../data/testing_midi_io.mid', 'wb') as output_file:
+    midi_writer = pm.FileWriter(output_file)
+    midi_writer.write(midi_object)
